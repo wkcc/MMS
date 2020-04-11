8ef555b804ae3e2b0023c04abd7664ba9ec2601a
wkcc <1045309691@qq.com>
2020-04-12 00:15:34 +0800

    up

--- demo-api/src/main/java/com/demo/controller/UserController.java
+++ demo-api/src/main/java/com/demo/controller/UserController.java
@@ -1,33 +1,36 @@
 package com.demo.controller;
 
-import com.demo.mapper.UserMapper;
+import com.demo.bo.UserBo;
 import com.demo.pojo.User;
 import com.demo.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("user")
+@CrossOrigin("http://127.0.0.1:8083")
 public class UserController {
     @Autowired
     private UserServiceImpl userService;
 
-    @GetMapping("/getUser")
+    @GetMapping("/get")
     public Object getUser(){
         List<User> users = userService.getUser();
-        return users.toString();
+        return users;
     }
 
-    @GetMapping("/getUserById")
-    public Object getUserById(String userId){
+    @GetMapping("/getById")
+    public Object getUserById(@RequestParam String userId){
         User user = (User) userService.getUserById(userId);
         return user.toString();
     }
 
-    @GetMapping("/addUser")
-    public void addUser(String userId, String userName, String userPassWord){
-        userService.addUser(userId, userName, userPassWord);
+    @PostMapping("/add")
+    public HttpStatus addUser(@RequestBody UserBo userBo){
+        userService.addUser(userBo.getUserId(), userBo.getUserName(), userBo.getUserPassWord());
+        return HttpStatus.OK;
     }
 }
--- demo-service/src/main/java/com/demo/service/impl/UserServiceImpl.java
+++ demo-service/src/main/java/com/demo/service/impl/UserServiceImpl.java
@@ -25,7 +25,7 @@ public class UserServiceImpl implements UserService {
     }
 
     public User getUserById(String userId) {
-        User user = userMapper.selectByPrimaryKey(userId);
+        User user = userMapper.getUserById(userId);
         return user;
     }
 }
